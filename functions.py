de2a52137d7c504acf0e234332005375e12611f4
MercybirungiS <mercyssbirungi@gmail.com>
2021-07-29 11:46:29 +0300

    functions

--- functions.py
+++ functions.py
@@ -1,12 +1,28 @@
+
+# Create a program that asks the user for a number and then prints out a list of all the divisors of that number. 
+num =int(input("enter a number :"))
+list=list(range(1,num+1))
+divisors=[]
+
+def find_divisors():
+	for number in list:
+		if num%number==0:
+			divisors.append(number)
+	print(divisors)
+find_divisors()
+
+
+
+
+
+
+
+
 # Ask the user for a number and determine whether the number is prime or not. 
-# (For those who have forgotten, a prime number is a number that has no divisors.). 
-#  Take this opportunity to practice using functions
-
-
 num = int(input('Insert a number: '))
 a = [x for x in range(2, num) if num % x == 0]
 
-def prime(n):
+def find_prime(n):
 	if num > 1:
 		if len(a) == 0:
 			print ('prime')
@@ -15,5 +31,5 @@ def prime(n):
 	else:
 		print ('not prime')
 	
-prime(num)
+find_prime(num)
 
