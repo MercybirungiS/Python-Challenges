af88055aa660b38228cd8ca32b348b93176c2f7e
MercybirungiS <mercyssbirungi@gmail.com>
2021-07-31 04:57:38 +0300

    functions

--- functions.py
+++ functions.py
@@ -1,16 +1,18 @@
 
 # Create a program that asks the user for a number and then prints out a list of all the divisors of that number. 
-num =int(input("enter a number :"))
-list=list(range(1,num+1))
-divisors=[]
 
-def find_divisors():
+# 8
+
+def find_divisors(num):
+	divisors=[]
+	list=(range(1,num+1))
 	for number in list:
 		if num%number==0:
 			divisors.append(number)
 	print(divisors)
-find_divisors()
-
+find_divisors(5)
+find_divisors(123)
+find_divisors(90)
 
 
 
@@ -19,10 +21,11 @@ find_divisors()
 
 
 # Ask the user for a number and determine whether the number is prime or not. 
-num = int(input('Insert a number: '))
-a = [x for x in range(2, num) if num % x == 0]
 
-def find_prime(n):
+
+
+def find_prime(num):
+	a = [x for x in range(2, num) if num % x == 0]
 	if num > 1:
 		if len(a) == 0:
 			print ('prime')
@@ -31,5 +34,9 @@ def find_prime(n):
 	else:
 		print ('not prime')
 	
-find_prime(num)
+find_prime(7)
+find_prime(345)
+find_prime(9)
+find_prime(80)
+
 
